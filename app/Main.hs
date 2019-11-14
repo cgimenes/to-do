8f56c814a17d6c808ffde53fe0c481677e823311
Marcelo Gimenes de Oliveira <mgimenes@uolinc.com>
2019-11-14 17:13:48 -0200

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Parsers
+import Commands
+import Types
+import Options.Applicative (execParser, info, progDesc)
+import Data.String.Utils (replace)
+import System.Directory (getHomeDirectory)
+
+main :: IO ()
+main = do
+    Options dataPath command <- execParser $ info optionsParser (progDesc "To-do list manager")
+    homeDir <- getHomeDirectory
+    let expandedDataPath = replace "~" homeDir dataPath
+    run expandedDataPath command
+
+run :: FilePath -> Command -> IO ()
+run dataPath Info = showInfo dataPath
+run dataPath Init = initItems dataPath
+run dataPath List = viewItems dataPath
+run dataPath (Add item) = addItem dataPath item
+run dataPath (View idx) = viewItem dataPath idx
+run dataPath (Update idx itemUpdate) = putStrLn $ "Update: idx=" ++ show idx ++ " itemUpdate=" ++ show itemUpdate
+run dataPath (Remove idx) = removeItem dataPath idx
